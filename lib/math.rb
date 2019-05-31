decddd4ee23d5077d9ad595e3fed7a2ffb51f9b3
Yosef Lehrer <yolehrer@gmail.com>
2019-05-31 10:18:06 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
 def addition(num1, num2)
+  1+8
 end
 
 def subtraction(num1, num2)
+  25-20
 end
 
 def division(num1, num2)
+  100/4
 end
 
 def multiplication(num1, num2)
+  1*120
 end
 
 def modulo(num1, num2)
+ 10%6
 end
 
 def square_root(num)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  7+((43*23)/87)
 end
