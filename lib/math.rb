95ecd41c31bedd62c1d25345b15219388c7569a7
adriennem76 <adrienne.c.miranda@gmail.com>
2019-06-16 14:07:17 -0400

    built methods

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,35 @@
 def addition(num1, num2)
+  sum = num1+num2
+  sum
 end
 
 def subtraction(num1, num2)
+  sum = num1 - num2
+  sum
 end
 
 def division(num1, num2)
+  sum = num1/num2
+  sum
 end
 
 def multiplication(num1, num2)
+  sum = num1 * num2
+  sum
 end
 
 def modulo(num1, num2)
+  remainder = num1 % num2
+  remainder
 end
 
 def square_root(num)
+  sum = Math.sqrt(num)
+  sum
 end
 
 def order_of_operation(num1, num2, num3, num4)
+  sum = num1 + ((num2 * num3) / num4)
+  sum
   #Hint:  __ + (( __ * __ ) / __ )
 end
